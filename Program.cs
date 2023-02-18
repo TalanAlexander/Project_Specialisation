a9ce09f34810c6ae24c7fd94d98dc5428c839224
AlexanderTalan <talan_ae@mail.ru>
2023-02-18 17:48:33 +0300

    CSS part is done!

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] people = { "Tom", "Sam", "Bobby", "Fy", "K", "Alex" };
+
+ShowArray(GetSameWords(people, 3));
+
+string [] GetSameWords (string [] array, int numberSigns)
+{
+    int lenOfNewArray = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= numberSigns)
+        {
+            lenOfNewArray ++;
+        }
+    }
+    
+    string[] newArray = new string[lenOfNewArray];
+
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= numberSigns)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+
+    return newArray;
+}
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
